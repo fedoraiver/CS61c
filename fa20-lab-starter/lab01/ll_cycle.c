3abddd1932e8a9d8bcf191fa258a68a4a4c64d4e
fedoraiver <cck1772416022@gmail.com>
2023-10-04 21:45:09 +0800

    finish lab01

--- fa20-lab-starter/lab01/ll_cycle.c
+++ fa20-lab-starter/lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node* tortoise=head;
+    node* hare=head;
+    while(tortoise!=NULL){
+        tortoise=tortoise->next;
+	if(hare!=NULL){
+	    hare=hare->next; 
+	    if(hare!=NULL){
+	        hare=hare->next;
+	    }else{
+	        return 0;
+	    }
+	}else{
+	    return 0;
+	}
+	if(hare==tortoise){
+	    return 1;
+	}
+    }
     return 0;
 }
